64be214259ecd17e4e1317bae0051645373a7fd5
Ryan Lee <ryanyinlee@protonmail.com>
2022-01-13 03:01:23 -0800

    should be working with heroku/postgres

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,10 +4,6 @@ const server = require('../src/server.js');
 const supertest = require('supertest');
 const request = supertest(server.app);
 
-
-// Update a record using PUT
-// Destroy a record using DELETE
-
 // TEST Create a record using POST
 describe('TEST Create a record using POST', () => {
 
@@ -23,7 +19,7 @@ describe('TEST Create a record using POST', () => {
 // Read a list of records using GET
 describe('TEST Get records using GET', () => {
 
-    it('Should be able to GET a record from the DB.', async () => {
+    it('Should be able to GET records from the DB.', async () => {
         let response = await request.get('/game');
 
         expect(response.status).toEqual(200);
@@ -32,18 +28,54 @@ describe('TEST Get records using GET', () => {
 })
 
 // Read a record using GET
-
 describe('TEST Get a single record using GET', () => {
 
     it('Should be able to GET a single record from the DB.', async () => {
-        let response = await request.get('/game/1');
 
+        let game = await request.get('/game');   
+        let gameId = game.body.results[0].id
+
+        let response = await request.get('/game');   
+                
+        //console.log("games object " + JSON.stringify(response.body.results[0].title));
+      
         expect(response.status).toEqual(200);
-        expect(response.body.title);
-        expect(response.body[0].id).toEqual(1);
+        expect(response.body.results[0].id).toEqual(gameId);
     })
 })
 
+// Update a record using POST
+describe('TEST Updating a single record using PATCH', () => {
+
+    it('Should be able to update a single record from the DB using PATCH.', async () => {
+
+        let game = await request.get('/game');   
+        let gameId = game.body.results[0].id
+
+        await request.patch('/game/'+gameId+'?title=test&releaseYear=9999');
+        let response = await request.get('/game/'+gameId);
+        expect(response.status).toEqual(200);
+        expect(response.body.results.title).toEqual('test');
+        expect(response.body.results.releaseYear).toEqual('9999');
+    })
+})
+
+// Destroy a record using DELETE
+describe('TEST Deleting a single record using DESTROY', () => {
+
+    it('Should be able to remove a single record from the DB using DESTROY.', async () => {
+        let game = await request.get('/game');   
+        let gameId = game.body.results[0].id
+        let response = await request.delete('/game/'+gameId);
+        expect(response.status).toEqual(200);
+        // let checkSpot = await request.get('/game/'+ gameId);
+       
+        // expect(checkSpot.body.results.title).toBe(null);
+    })
+})
+
+
+// 404 Tests
 describe('Testing the 404 for routes!', () => {
 
     it('Should give a 404 on a bad route.', async () => {
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const {start} = require('./src/server.js');
 const {db} = require('./src/models');
 const server = require('./src/server.js');
+const POSTGRES_URI = process.env.DATABASE_URL;
 
 //server.start(process.env.PORT || 3000);
 
